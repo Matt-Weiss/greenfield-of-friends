be6fd13dd0b5799f2755dc9fc9f08820f47f7695
Matt Weiss <weiss.matt@outlook.com>
2019-05-10 21:13:22 -0600

    adds migration for friendships

--- db/migrate/20190510042713_create_friendships.rb
+++ db/migrate/20190510042713_create_friendships.rb
@@ -1,4 +1,10 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
+    create_table :friendships do |t|
+      t.integer :initiator_github_id
+      t.integer :recipient_github_id
+
+      t.timestamps
+    end
   end
 end
